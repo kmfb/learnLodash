5139ae27083a6a10313f5cdde4f5e7ddbc64131e
Luke <zhaotianxion@gmail.com>
2019-03-29 16:22:03 +0800

    add slice

---
+++ slice.js
@@ -0,0 +1,46 @@
+/* 
+    @param {Array} array
+    @param {Number} [start=0]
+    @param {Number} [end=array.length] 
+    @returns {Array} 
+*/
+
+// 1. 检验输入参数
+// 2. 转换start和end
+// 3. 获取输出数组的length
+// 4. 新建数组，并将切边数组的元素放入
+// 5. 返回切片数组
+
+const slice = (array, start, end) => {
+    let length = array.length ? array.length : 0
+    if (!length) {
+        return []
+    }
+
+    start = start ? start : 0
+    end = end ? end : length
+
+    if (start < 0) {
+        start = -start > length ? 0 : (length + start)
+    }
+
+    if (end < 0) {
+        end = -end > length ? length : (length + end)
+    }
+
+    length = start > end ? 0 : (end - start)
+
+    const result = new Array(length)
+    let index = -1
+
+    while (++index < length) {
+        result[index] = array[index + start]
+    }
+
+    return result
+
+}
+
+const arr = [1, 2, 3, 4]
+const result = slice(arr)
+console.log(result) 
